8b8a705866cad6665d25e331b1a253d9efe10648
João Albieri <jtoscanalbieri@gmail.com>
2025-01-25 15:16:02 -0300

    add files

---
+++ src/test/java/br/com/API/gerenciamento_contatos/GerenciamentoContatosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.API.gerenciamento_contatos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentoContatosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
